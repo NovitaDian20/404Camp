ce7f7b6cf6785949bb7e2c730536196542b31b95
NovitaDian20 <anugraheninovita04@gmail.com>
2021-10-18 03:35:25 +0700

    branching and loop schema

---
+++ branching.py
@@ -0,0 +1,13 @@
+kkunci=input("Password :")
+if(kkunci=="tes123") :
+    print("You have success log in")
+    print("CHECK ODD NUMBER OR EVEN NUMBER ")
+    number=int(input("Number:"))
+    if(number%2==0) :
+        print("This number" , number, "is even")
+    else :
+        print("This number",number,"is odd number")
+        
+else: 
+    print("You have to login first")
+    
---
+++ loop.py
@@ -0,0 +1,17 @@
+
+#for x in range(10):
+#   print(x)
+    
+#Knowledge 
+#Schema:
+#for c in range(max)
+#   action
+#for c in range (min, max)
+#   action
+
+for y in range(0):
+    print(y)
+for x in range(6,8):
+    print(x)
+for z in range(-10,-2):
+    print(z, end=" ")
