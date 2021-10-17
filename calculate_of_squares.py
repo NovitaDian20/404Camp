7b7891025568f92fd23d8decda83fc76b926daae
NovitaDian20 <anugraheninovita04@gmail.com>
2021-10-18 02:58:55 +0700

    calculate square

---
+++ calculate_of_squares.py
@@ -0,0 +1,9 @@
+print("Calculates 3 squares has different values")
+square1=int(input("side 1 :"))
+square2=int(input("side 2 : "))
+square3=int(input("side 3 :"))
+area1=square1*square1
+area2=square2*square2
+area3=square3*square3
+total=area1+area2+area3
+print("Total area of the squares :{:.2f}", total)
